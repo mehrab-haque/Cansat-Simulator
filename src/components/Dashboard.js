b0bc57284c253d82d213e10274e8f9d534c36a66
mehrab-haque <mehrab.haque.0001@gmail.com>
2022-09-01 13:48:28 +0600

    sensors done

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -12,13 +12,49 @@ const Dashboard=props=>{
     const dbRef = ref(db);
 
     const [altitudeGraph, setAltitudeGraph]=useState({
-        labels: ["t+14", "t+13", "t+12","t+11","t+10","t+8","t+7","t+7","t+6","t+5","t+4","t+3","t+2","t+1","t"],
+        labels: ["t-14", "t-13", "t-12","t-11","t-10","t-8","t-7","t-7","t-6","t-5","t-4","t-3","t-2","t-1","t"],
         datasets: [
             {
-                label: "Cansat Altitude",
+                label: "Cansat Altitude (m)",
                 data: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
                 fill: false,
-                borderColor: "#742774"
+                borderColor: "#0075cc"
+            }
+        ]
+    })
+
+    const [tempGraph, setTempGraph]=useState({
+        labels: ["t-14", "t-13", "t-12","t-11","t-10","t-8","t-7","t-7","t-6","t-5","t-4","t-3","t-2","t-1","t"],
+        datasets: [
+            {
+                label: "Temperature (C)",
+                data: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                fill: false,
+                borderColor: "#e8001a"
+            }
+        ]
+    })
+
+    const [humGraph, setHumGraph]=useState({
+        labels: ["t-14", "t-13", "t-12","t-11","t-10","t-8","t-7","t-7","t-6","t-5","t-4","t-3","t-2","t-1","t"],
+        datasets: [
+            {
+                label: "Humidity (%)",
+                data: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                fill: false,
+                borderColor: "#07b200"
+            }
+        ]
+    })
+
+    const [barGraph, setBarGraph]=useState({
+        labels: ["t-14", "t-13", "t-12","t-11","t-10","t-8","t-7","t-7","t-6","t-5","t-4","t-3","t-2","t-1","t"],
+        datasets: [
+            {
+                label: "Barometric Pressure (kPa)",
+                data: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                fill: false,
+                borderColor: "#cbb800"
             }
         ]
     })
@@ -32,10 +68,27 @@ const Dashboard=props=>{
     useEffect(()=>{
         onValue(dbRef, (snapshot) => {
             const data = snapshot.val();
+
             var tmpAlt={...altitudeGraph}
             tmpAlt['datasets'][0].data=arrayRotate(tmpAlt['datasets'][0].data)
             tmpAlt['datasets'][0].data[tmpAlt['datasets'][0].data.length-1]=data.altitude
             setAltitudeGraph(tmpAlt)
+
+            var tmpAlt1={...tempGraph}
+            tmpAlt1['datasets'][0].data=arrayRotate(tmpAlt1['datasets'][0].data)
+            tmpAlt1['datasets'][0].data[tmpAlt1['datasets'][0].data.length-1]=data.temp
+            setTempGraph(tmpAlt1)
+
+            var tmpAlt2={...humGraph}
+            tmpAlt2['datasets'][0].data=arrayRotate(tmpAlt2['datasets'][0].data)
+            tmpAlt2['datasets'][0].data[tmpAlt2['datasets'][0].data.length-1]=data.hum
+            setHumGraph(tmpAlt2)
+
+            var tmpAlt3={...barGraph}
+            tmpAlt3['datasets'][0].data=arrayRotate(tmpAlt3['datasets'][0].data)
+            tmpAlt3['datasets'][0].data[tmpAlt3['datasets'][0].data.length-1]=data.bar
+            setBarGraph(tmpAlt3)
+
         },error=>{
             console.log(error)
         });
@@ -51,6 +104,25 @@ const Dashboard=props=>{
                 <Line data={altitudeGraph} options={{
                     maintainAspectRatio: false,
                 }}/>
+            </Grid>
+            <Grid style={{height:'33vh'}} item xs={12} md={4}>
+                <Grid container spacing={1} >
+                    <Grid style={{height:'33vh'}} item xs={12}>
+                        <Line data={tempGraph} options={{
+                            maintainAspectRatio: false,
+                        }}/>
+                    </Grid>
+                    <Grid style={{height:'33vh'}} item xs={12}>
+                        <Line data={humGraph} options={{
+                            maintainAspectRatio: false,
+                        }}/>
+                    </Grid>
+                    <Grid style={{height:'33vh'}} item xs={12}>
+                        <Line data={barGraph} options={{
+                            maintainAspectRatio: false,
+                        }}/>
+                    </Grid>
+                </Grid>
             </Grid>
         </Grid>
     )
--- src/components/Simulator.js
+++ src/components/Simulator.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import {Grid, Paper, Slider, Typography} from "@mui/material";
-import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, ref, set,update } from "firebase/database";
 
 
 const Simulator=props=>{
     const db = getDatabase();
 
     const handleAltChange=(e,val)=>{
-        set(ref(db), {
+        update(ref(db), {
             altitude:val
+        });
+    }
+
+    const handleTempChange=(e,val)=>{
+        update(ref(db), {
+            temp:val
+        });
+    }
+
+    const handleHumidityChange=(e,val)=>{
+        update(ref(db), {
+            hum:val
+        });
+    }
+
+    const handleBarChange=(e,val)=>{
+        update(ref(db), {
+            bar:val
         });
     }
 
@@ -26,6 +44,60 @@ const Simulator=props=>{
                                 defaultValue={80}
                                 step={1}
                                 onChangeCommitted={handleAltChange}
+                                valueLabelDisplay={"on"}
+                            />
+                        </div>
+                    </Typography>
+                </Paper>
+            </Grid>
+            <Grid item md={4} xs={12}>
+                <Paper style={{padding:'10px'}}>
+                    <Typography variant={'h6'}>
+                        <center>
+                            Temmperature (Celcius)
+                        </center>
+                        <div>
+                            <Slider
+                                aria-label="Always visible"
+                                defaultValue={80}
+                                step={1}
+                                onChangeCommitted={handleTempChange}
+                                valueLabelDisplay={"on"}
+                            />
+                        </div>
+                    </Typography>
+                </Paper>
+            </Grid>
+            <Grid item md={4} xs={12}>
+                <Paper style={{padding:'10px'}}>
+                    <Typography variant={'h6'}>
+                        <center>
+                            Humidity (%)
+                        </center>
+                        <div>
+                            <Slider
+                                aria-label="Always visible"
+                                defaultValue={80}
+                                step={1}
+                                onChangeCommitted={handleHumidityChange}
+                                valueLabelDisplay={"on"}
+                            />
+                        </div>
+                    </Typography>
+                </Paper>
+            </Grid>
+            <Grid item md={4} xs={12}>
+                <Paper style={{padding:'10px'}}>
+                    <Typography variant={'h6'}>
+                        <center>
+                            Pressure (kPa)
+                        </center>
+                        <div>
+                            <Slider
+                                aria-label="Always visible"
+                                defaultValue={80}
+                                step={1}
+                                onChangeCommitted={handleBarChange}
                                 valueLabelDisplay={"on"}
                             />
                         </div>
